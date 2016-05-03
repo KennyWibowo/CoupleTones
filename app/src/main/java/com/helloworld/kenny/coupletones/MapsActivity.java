e91edb1b54ee673bf99ddd04caaf47bc6724d390
TatteredEye <varun.suj@gmail.com>
2016-05-03 01:34:21 -0700

    Added Remove Favorite Location, Added Favorite Location on Map Press, Fixed Side Menu

--- app/src/main/java/com/helloworld/kenny/coupletones/MapsActivity.java
+++ app/src/main/java/com/helloworld/kenny/coupletones/MapsActivity.java
@@ -8,6 +8,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationListener;
+import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -17,6 +18,9 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.*;
 //import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -37,18 +41,26 @@ import java.io.IOException;
 import java.util.List;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
     private EditText searchText;
     private MapsActivity me;
+    private ListView lists;
     private ArrayList<String> storage = new ArrayList<String>();
     private UiSettings myUiSetting;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        lists = (ListView) findViewById(R.id.left_drawer);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -59,6 +71,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         me = this;
 
         searchText.addTextChangedListener(watcher);
+
 
 //<<<<<<< HEAD
         //final MapView view = (MapView) findViewById(R.id.map);
@@ -116,13 +129,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //=======
 //>>>>>>> 955019d80d6dc422fc58077e145f4304b894b52e
 //>>>>>>> 219c11c12e7f371e8544aec1dff411a1b2bd13e4
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
-    public void addLocation(View view) {
+    public void removeLocation(View view) {
+        if(storage.size() != 0) {
 
+            storage.remove(storage.size() - 1);
+            ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, storage);
+            lists.setAdapter(adapt);
+        }
 
     }
+
+    @Override
+    public void onMapClick(LatLng point) {
+        System.out.println("Hello World!");
+
+        storage.add(Double.toString(point.latitude) + " " + Double.toString(point.longitude));
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, storage);
+        lists.setAdapter(adapt);
+
+    }
+
+
 
     /**
      * Manipulates the map once available.
@@ -152,10 +185,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(this);
 
         myUiSetting = mMap.getUiSettings();
         myUiSetting.setZoomControlsEnabled(true);
-
 
 
     }
@@ -167,25 +200,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             String location = searchText.getText().toString();
             List<Address> addressList = null;
 
-            if(location != null || !location.equals(""))
-            {
+            if (location != null || !location.equals("")) {
                 Geocoder geocoder = new Geocoder(me);
                 try {
-                    addressList = geocoder.getFromLocationName(location , 1);
+                    addressList = geocoder.getFromLocationName(location, 1);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
 
-                if(addressList.size() > 0) {
+                if (addressList.size() > 0) {
                     Address address = addressList.get(0);
-                    LatLng latLng = new LatLng(address.getLatitude() , address.getLongitude());
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
                     mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
                 }
             }
         }
 
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
 
-        public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+        }
     };
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.helloworld.kenny.coupletones/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Maps Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.helloworld.kenny.coupletones/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
