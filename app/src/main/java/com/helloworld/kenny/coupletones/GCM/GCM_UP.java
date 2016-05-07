67f8aa554864c7a5c685542621a8e95dfb94c011
tengzhou <znspirationist@gmail.com>
2016-05-06 19:09:47 -0700

    pull

---
+++ app/src/main/java/com/helloworld/kenny/coupletones/GCM/GCM_UP.java
@@ -0,0 +1,7 @@
+package com.helloworld.kenny.coupletones.GCM;
+
+/**
+ * Created by tengzhou on 5/6/16.
+ */
+public class GCM_UP {
+}
--- app/src/main/java/com/helloworld/kenny/coupletones/MapsActivity.java
+++ app/src/main/java/com/helloworld/kenny/coupletones/MapsActivity.java
@@ -362,7 +362,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         namePrompt.show();
     }
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
