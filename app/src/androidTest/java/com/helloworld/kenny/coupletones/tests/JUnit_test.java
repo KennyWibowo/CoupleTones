b2c6f6f1d891b574b177de6934ee978bed298568
TatteredEye <varun.suj@gmail.com>
2016-05-08 15:19:02 -0700

    Added Test Cases for Favorites Storage

--- app/src/androidTest/java/com/helloworld/kenny/coupletones/tests/JUnit_test.java
+++ app/src/androidTest/java/com/helloworld/kenny/coupletones/tests/JUnit_test.java
@@ -2,7 +2,12 @@ package com.helloworld.kenny.coupletones.tests;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.helloworld.kenny.coupletones.MapsActivity;
+import com.helloworld.kenny.coupletones.favorites.FavoriteEntry;
+import com.helloworld.kenny.coupletones.favorites.Favorites;
+import com.helloworld.kenny.coupletones.favorites.exceptions.InvalidNameException;
+import com.helloworld.kenny.coupletones.favorites.exceptions.NameInUseException;
 
 /**
  * Created by Karen on 5/8/2016.
@@ -15,9 +20,55 @@ public class JUnit_test extends ActivityInstrumentationTestCase2<MapsActivity> {
         super(MapsActivity.class);
     }
 
-    public void test() {
-        assertEquals(2+2, 4);
+    public void test_add() throws Exception
+    {
+        Favorites favorites = new Favorites();
+        boolean check = false;
+        favorites.addEntry("Library", new LatLng(45, 45));
+        try {
+
+            favorites.addEntry("Library", new LatLng(45, 45));
+        } catch (NameInUseException ex) {
+            check = true;
+            assertTrue(check);
+        }
+        assertTrue(check);
     }
 
+    public void test_size() throws Exception
+    {
+        Favorites favorites = new Favorites();
+        favorites.addEntry("Library", new LatLng(45, 45));
+        favorites.addEntry("Church", new LatLng(25, 65));
+        favorites.addEntry("Supermarket", new LatLng(83, 29));
+        favorites.addEntry("Class", new LatLng(32, 24));
+        favorites.addEntry("Home", new LatLng(335, 554));
+        favorites.addEntry("Restaurant", new LatLng(934, 753));
+        favorites.addEntry("Fair", new LatLng(675, 875));
+        assertEquals(7, favorites.size());
+    }
+
+
+    public void test_entry() throws Exception
+    {
+        Favorites favorites = new Favorites();
+        favorites.addEntry("Library", new LatLng(45, 45));
+        favorites.addEntry("Church", new LatLng(25, 65));
+        favorites.addEntry("Supermarket", new LatLng(83, 29));
+        favorites.addEntry("Class", new LatLng(32, 24));
+        favorites.addEntry("Home", new LatLng(335, 554));
+        favorites.addEntry("Restaurant", new LatLng(934, 753));
+        favorites.addEntry("Fair", new LatLng(675, 875));
+        favorites.deleteEntry(0);
+        assertEquals("Church", favorites.getEntry(0).getName());
+    }
+
+    public void test_exist() throws Exception
+    {
+        Favorites favorites = new Favorites();
+        FavoriteEntry list = new FavoriteEntry("Taco Bell", new LatLng(68, 68));
+        favorites.addEntry("Taco Bell", new LatLng(68, 68));
+        assertEquals(list.toString(), favorites.getEntry(0).toString());
+    }
 
 }
