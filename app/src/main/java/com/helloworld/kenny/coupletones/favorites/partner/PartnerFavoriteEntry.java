8eeaafef8979d3c68369880db16413bec56e043c
Kenny Wibowo <kewibowo@ucsd.edu>
2016-05-24 16:29:50 -0700

    added new PartnerFavoriteEntry to handle vibrations

---
+++ app/src/main/java/com/helloworld/kenny/coupletones/favorites/partner/PartnerFavoriteEntry.java
@@ -0,0 +1,15 @@
+package com.helloworld.kenny.coupletones.favorites.partner;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.helloworld.kenny.coupletones.favorites.Entry;
+
+/**
+ * Created by Kenny on 5/24/2016.
+ */
+public class PartnerFavoriteEntry extends Entry {
+    public PartnerFavoriteEntry(String name, LatLng location) {
+        super(name, location);
+    }
+
+    //TODO: add functionality to bind tones/vibrations
+}
