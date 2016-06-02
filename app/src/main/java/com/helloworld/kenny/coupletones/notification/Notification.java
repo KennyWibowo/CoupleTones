5a55e7dd6b12d26e81fbb477fe58748b28439101
Kenny Wibowo <kewibowo@ucsd.edu>
2016-06-01 23:15:40 -0700

    Init basic notification abstract class

---
+++ app/src/main/java/com/helloworld/kenny/coupletones/notification/Notification.java
@@ -0,0 +1,13 @@
+package com.helloworld.kenny.coupletones.notification;
+
+import android.content.Context;
+
+/**
+ * Created by Kenny on 6/1/2016.
+ */
+public abstract class Notification {
+
+    public abstract void play();
+
+    public abstract String toString();
+}
