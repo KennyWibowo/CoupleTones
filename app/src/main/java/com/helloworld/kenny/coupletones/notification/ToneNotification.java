52b55858cca176b566f5a414b2d7c3f18e03f1ac
TatteredEye <varun.suj@gmail.com>
2016-06-03 19:06:41 -0700

    Added Media thread

--- app/src/main/java/com/helloworld/kenny/coupletones/notification/ToneNotification.java
+++ app/src/main/java/com/helloworld/kenny/coupletones/notification/ToneNotification.java
@@ -1,12 +1,15 @@
 package com.helloworld.kenny.coupletones.notification;
 
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.media.audiofx.BassBoost;
 import android.net.Uri;
+import android.os.IBinder;
 
+import com.helloworld.kenny.coupletones.MapsActivity;
 import com.helloworld.kenny.coupletones.settings.Settings;
 
 /**
@@ -16,23 +19,25 @@ public class ToneNotification extends Notification {
 
     private String name;
     Ringtone tone;
+    Context context;
     Ringtone standardTone;
 
 
 
-    public ToneNotification(String name, Ringtone tone, Ringtone standardTone) {
+    public ToneNotification(String name, Ringtone tone, Ringtone standardTone, Context context) {
         this.name = name;
         this.tone = tone;
+        this.context = context;
         this.standardTone = standardTone;
     }
 
 
     public void play() {
+
+        Intent intent = new Intent(context, MediaService.class);
+        context.startService(intent);
         if(Settings.tonesEnabled()) {
             standardTone.play();
-            if (tone != null) {
-                tone.play();
-            }
         }
     }
 
@@ -43,6 +48,40 @@ public class ToneNotification extends Notification {
 
     public Ringtone getTone() {
         return standardTone;
+    }
+
+    class MediaService extends Service{
+
+        public MediaService(){
+
+        }
+
+        public int onStartCommand(Intent intent, int flags, int startId){
+            synchronized (this)
+            {
+                try{
+                    wait(2000);
+                    if (tone != null) {
+                        tone.play();
+                    }
+
+                }
+                catch (InterruptedException w){
+                    w.printStackTrace();
+                }
+            }
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        public IBinder onBind(Intent intent){
+            throw new UnsupportedOperationException("Not yet");
+        }
+
+        public void onDestroy() {
+            super.onDestroy();
+
+        }
+
     }
 
 }
